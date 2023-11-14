3c8e65636be1bc374a470dfb2df2c271a28f6f81
akaboshinit <akaboshinit@gmail.com>
2023-11-14 15:15:35 +0900

    使えるよう初期設定

---
+++ packages/uid/bin/uid.dart
@@ -0,0 +1,5 @@
+import 'package:uid/uid.dart' as uid;
+
+void main(List<String> arguments) {
+  print('Hello world: ${uid.calculate()}!');
+}
---
+++ packages/uid/test/uid_test.dart
@@ -0,0 +1,8 @@
+import 'package:uid/uid.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
