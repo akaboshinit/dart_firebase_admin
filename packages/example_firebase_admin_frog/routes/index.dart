be9a3883f6879f1ec9f5a31596104cbba527f363
akaboshinit <akaboshinit@gmail.com>
2023-11-16 00:16:54 +0900

    firebase_admin_frogの実装

---
+++ packages/example_firebase_admin_frog/routes/index.dart
@@ -0,0 +1,39 @@
+import 'dart:io';
+
+// import 'package:dart_firebase_admin/auth.dart';
+import 'package:dart_firebase_admin/dart_firebase_admin.dart';
+import 'package:dart_firebase_admin/firestore.dart';
+import 'package:dart_frog/dart_frog.dart';
+
+Future<Response> onRequest(RequestContext context) async {
+  final serviceAccountFile = File('service-account.json');
+  final admin = FirebaseAdminApp.initializeApp(
+    'template-dev-d032e',
+    Credential.fromServiceAccount(serviceAccountFile),
+  );
+
+  // admin.useEmulator();
+
+  // final auth =Auth(admin);
+
+  final firestore = Firestore(admin);
+  final collection = firestore.collection('test');
+
+  await collection.doc(DateTime.now().toString()).set({
+    'age': 32,
+    'time': DateTime.now().toIso8601String(),
+  });
+
+  final snapshot = await collection.get();
+
+  final data = <Map<String, Object?>>[];
+  for (final doc in snapshot.docs) {
+    data.add(doc.data());
+  }
+
+  await admin.close();
+
+  return Response(
+    body: 'Welcome to Dart Frog!! ${data.map((e) => e.toString())}}',
+  );
+}
